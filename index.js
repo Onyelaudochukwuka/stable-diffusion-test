40feb468304832fdcecff9dc50337c7a6d349ced
Udoka <udochukwukaonyela@gmail.com>
2022-11-11 11:20:40 +0100

    formatted

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-require('dotenv').config();
-const stability = require('stability-client');
-const uploadImage = require('./cloudinary');
+require("dotenv").config();
+const stability = require("stability-client");
+const uploadImage = require("./cloudinary");
 // generate an image
 const api = stability.generate({
-    width: 1024,
-    height: 1024,
-    format: 'png',
-    prompt: "A cartoon House",
-    apiKey: process.env.DREAMSTUDIO_API_KEY,
-    outDir: "./images",
-    
+  width: 1024,
+  height: 1024,
+  format: "png",
+  prompt: "A cartoon House",
+  apiKey: process.env.DREAMSTUDIO_API_KEY,
+  outDir: "./images",
 });
 
-api.on('image', ({ buffer, filePath }) => {
-    uploadImage(filePath).then(console.log);
-})
+api.on("image", ({ buffer, filePath }) => {
+  uploadImage(filePath).then(console.log);
+});
 
-api.on('end', (data) => {
-    console.log('Generating Complete', data)
-})
+api.on("end", data => {
+  console.log("Generating Complete", data);
+});
